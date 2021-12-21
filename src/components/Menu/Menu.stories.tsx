9b223e28d433979a617584e3db6f4cc834fe2195
Long Nguyen The Xuan <longntx@tech.est-rouge.com>
2021-12-21 15:50:12 +0700

    Add basic structure

---
+++ src/components/Menu/Menu.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Menu from "./Menu";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: "ReactComponentLibrary/Menu",
+    component: Menu,
+} as ComponentMeta<typeof Menu>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Menu> = (args) => <Menu {...args} />;
+
+export const HelloWorld = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+HelloWorld.args = {
+    label: "Hello world!",
+};
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,70 @@
-import React from "react";
+import React from 'react';
+import './Menu.css';
+import {
+  BiBarChartAlt2,
+  BiBookmark,
+  BiFolder,
+  BiGridAlt,
+  BiLayer,
+  BiLogOut,
+  BiMessageSquareDetail,
+  BiUser,
+} from 'react-icons/bi';
 
 export interface MenuProps {
-    label: string;
+  label: string;
 }
 
 const Menu = (props: MenuProps) => {
-    return <button>{props.label}</button>;
+  return (
+    <div className="l-navbar" id="nav-bar">
+      <nav className="nav">
+        <div>
+          <a href="#" className="nav_logo">
+            {' '}
+            <BiLayer className="nav_logo-icon"></BiLayer>{' '}
+            <span className="nav_logo-name">BBBootstrap</span>{' '}
+          </a>
+          <div className="nav_list">
+            <a href="#" className="nav_link active">
+              {' '}
+              <BiGridAlt className="nav-icon"></BiGridAlt>{' '}
+              <span className="nav_name">Dashboard</span>{' '}
+            </a>{' '}
+            <a href="#" className="nav_link">
+              {' '}
+              <BiUser className="nav-icon"></BiUser>{' '}
+              <span className="nav_name">Users</span>{' '}
+            </a>{' '}
+            <a href="#" className="nav_link">
+              {' '}
+              <BiMessageSquareDetail className="nav-icon"></BiMessageSquareDetail>{' '}
+              <span className="nav_name">Messages</span>
+            </a>{' '}
+            <a href="#" className="nav_link">
+              {' '}
+              <BiBookmark className="nav-icon"></BiBookmark>{' '}
+              <span className="nav_name">Bookmark</span>{' '}
+            </a>{' '}
+            <a href="#" className="nav_link">
+              {' '}
+              <BiFolder className="nav-icon"></BiFolder>{' '}
+              <span className="nav_name">Files</span>{' '}
+            </a>{' '}
+            <a href="#" className="nav_link">
+              <BiBarChartAlt2 className="nav-icon"></BiBarChartAlt2>{' '}
+              <span className="nav_name">Stats</span>{' '}
+            </a>
+          </div>
+        </div>
+        <a href="#" className="nav_link">
+          {' '}
+          <BiLogOut className="nav-icon"></BiLogOut>{' '}
+          <span className="nav_name">SignOut</span>{' '}
+        </a>
+      </nav>
+    </div>
+  );
 };
 
 export default Menu;
